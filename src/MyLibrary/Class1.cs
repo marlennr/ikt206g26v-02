d4977904813d240f619b242bcf6a5c2f8a858835
Marlen Nyman Rogneflåten <marlennr@uia.no>
2026-02-02 20:44:33 +0100

    class library and unit test

---
+++ src/MyLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary;
+
+public class Class1
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
---
+++ tests/MyLibrary.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using MyLibrary;
+
+namespace MyLibrary.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var obj = new Class1();
+        var result = obj.Add(2, 3);
+
+        Assert.Equal(5, result);
+    }
+}
